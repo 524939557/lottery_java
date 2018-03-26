b5f8a80113be8e9190868d6d394e1ae51a1c3081
little_star <524939557@qq.com>
2018-03-26 16:53:38 +0800

    last

--- src/main/java/com/homeene/alibaba/demo/Env.java
+++ src/main/java/com/homeene/alibaba/demo/Env.java
@@ -6,7 +6,7 @@ public class Env {
 	
 	public static final String OAPI_HOST = "https://oapi.dingtalk.com";
 	public static final String OA_BACKGROUND_URL = ""; 
-	public static final String CORP_ID = "ding8251484bf665825b35c2f4657eb6378f";
+	public static final String CORP_ID = "client_credential";
 	
 	public static final String CORP_SECRET = "oRIsb5QwdhKLlueYFWU6WuF1eryx26NFK09Ro4ladXEX5HXn9ZQjRr4pADKmRN1r";
 	public static final String SSO_Secret = "";
